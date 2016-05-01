08897d37058006728dd294667e6a008dce1ac56e
Anton_Havrylenko <anton.havrylenko@gmail.com>
2016-05-01 23:16:16 +0300

    Add CoreDataMigration

---
+++ CoreDataMigration/Lesson34/Opinion.swift
@@ -0,0 +1,15 @@
+//
+//  Opinion.swift
+//  Lesson34
+//
+//  Created by Anton Havrylenko on 25.04.16.
+//  Copyright © 2016 Havrylenko. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Opinion: NSManagedObject {
+
+}
